d2640edb5924447990be8f1e529fd0ac4a3c1580
Chris Kontos <chris.kontos@wearespindle.com>
2019-02-26 17:24:03 +0100

    VIALI-3578:fixed the crash after first dial and call

--- Pod/Classes/VSLAccount.m
+++ Pod/Classes/VSLAccount.m
@@ -51,7 +51,7 @@ static NSString * const VSLAccountErrorDomain = @"VialerSIPLib.VSLAccount";
 - (void)setAccountState:(VSLAccountState)accountState {
     if (_accountState != accountState) {
         VSLLogDebug(@"AccountState will change from %@(%ld) to %@(%ld)", VSLAccountStateString(_accountState),
-                   (long)_accountState, VSLAccountStateString(accountState), (long)accountState);
+                    (long)_accountState, VSLAccountStateString(accountState), (long)accountState);
         _accountState = accountState;
     }
 }
@@ -66,7 +66,7 @@ static NSString * const VSLAccountErrorDomain = @"VialerSIPLib.VSLAccount";
     }
     pjsua_acc_info accountInfo;
     pj_status_t status;
-
+    
     status = pjsua_acc_get_info((pjsua_acc_id)self.accountId, &accountInfo);
     if (status != PJ_SUCCESS) {
         return 0;
@@ -78,10 +78,10 @@ static NSString * const VSLAccountErrorDomain = @"VialerSIPLib.VSLAccount";
     if (!self.isAccountValid) {
         return -1;
     }
-
+    
     pjsua_acc_info accountInfo;
     pj_status_t status;
-
+    
     status = pjsua_acc_get_info((pjsua_acc_id)self.accountId, &accountInfo);
     if (status != PJ_SUCCESS) {
         return -1;
@@ -102,10 +102,10 @@ static NSString * const VSLAccountErrorDomain = @"VialerSIPLib.VSLAccount";
     if ([[VSLEndpoint sharedEndpoint].endpointConfiguration hasTLSConfiguration]) {
         transportString = @";transport=tls";
     }
-
+    
     pjsua_acc_config acc_cfg;
     pjsua_acc_config_default(&acc_cfg);
-
+    
     // Add sip information to the pjsua account configuration.
     acc_cfg.id = [[accountConfiguration.sipAddress stringByAppendingString:transportString] prependSipUri].pjString;
     acc_cfg.reg_uri = [[accountConfiguration.sipDomain stringByAppendingString:transportString] prependSipUri].pjString;
@@ -113,7 +113,7 @@ static NSString * const VSLAccountErrorDomain = @"VialerSIPLib.VSLAccount";
     acc_cfg.publish_enabled = accountConfiguration.sipPublishEnabled ? PJ_TRUE : PJ_FALSE;
     acc_cfg.reg_timeout = VSLAccountRegistrationTimeoutInSeconds;
     acc_cfg.drop_calls_on_reg_fail = accountConfiguration.dropCallOnRegistrationFailure ? PJ_TRUE : PJ_FALSE;
-
+    
     // Add account information to the pjsua account configuration.
     acc_cfg.cred_count = 1;
     acc_cfg.cred_info[0].scheme = accountConfiguration.sipAuthScheme.pjString;
@@ -122,42 +122,42 @@ static NSString * const VSLAccountErrorDomain = @"VialerSIPLib.VSLAccount";
     acc_cfg.cred_info[0].data_type = PJSIP_CRED_DATA_PLAIN_PASSWD;
     acc_cfg.cred_info[0].data = accountConfiguration.sipPassword.pjString;
     acc_cfg.proxy_cnt = 0;
-
+    
     // If a proxy server is present on the account configuration add this to pjsua account configuration.
     if (accountConfiguration.sipProxyServer) {
         acc_cfg.proxy_cnt = 1;
         acc_cfg.proxy[0] = [[accountConfiguration.sipProxyServer stringByAppendingString:transportString] prependSipUri].pjString;
     }
-
+    
     acc_cfg.sip_stun_use = accountConfiguration.sipStunType;
     acc_cfg.media_stun_use = accountConfiguration.mediaStunType;
-
+    
     acc_cfg.allow_via_rewrite = accountConfiguration.allowViaRewrite ? PJ_TRUE : PJ_FALSE;
     acc_cfg.allow_contact_rewrite = accountConfiguration.allowContactRewrite ? PJ_TRUE : PJ_FALSE;
-
+    
     // Only set the contact rewrite method when allow contact rewrite is set to TRUE.
     if (accountConfiguration.allowContactRewrite) {
         acc_cfg.contact_rewrite_method = accountConfiguration.contactRewriteMethod;
     }
-
+    
     if ([[VSLEndpoint sharedEndpoint].endpointConfiguration hasTCPConfiguration] || [[VSLEndpoint sharedEndpoint].endpointConfiguration hasTLSConfiguration]) {
         VSLIpChangeConfiguration *ipChangeConfiguration = [VSLEndpoint sharedEndpoint].endpointConfiguration.ipChangeConfiguration;
         if (ipChangeConfiguration) {
             // Shutdown the old transport is no longer connected because of an ip address change.
             acc_cfg.ip_change_cfg.shutdown_tp = ipChangeConfiguration.ipAddressChangeShutdownTransport ? PJ_TRUE : PJ_FALSE;
-
+            
             // Don't hangup calls when the ip address changes.
             acc_cfg.ip_change_cfg.hangup_calls = ipChangeConfiguration.ipAddressChangeHangupAllCalls ? PJ_TRUE : PJ_FALSE;
-
+            
             // When a call is reinvited use the specified header.
             if (!ipChangeConfiguration.ipAddressChangeHangupAllCalls) {
                 acc_cfg.ip_change_cfg.reinvite_flags = (unsigned int)ipChangeConfiguration.ipAddressChangeReinviteFlags;
             }
-
+            
         }
         acc_cfg.contact_use_src_port = accountConfiguration.contactUseSrcPort ? PJ_TRUE : PJ_FALSE;
     }
-
+    
     if ([[VSLEndpoint sharedEndpoint].endpointConfiguration hasTLSConfiguration]) {
         acc_cfg.srtp_secure_signaling = 1;
         acc_cfg.use_srtp = PJMEDIA_SRTP_MANDATORY;
@@ -168,7 +168,7 @@ static NSString * const VSLAccountErrorDomain = @"VialerSIPLib.VSLAccount";
         acc_cfg.turn_cfg.enable_turn = accountConfiguration.turnConfiguration.enableTurn;
         acc_cfg.turn_cfg.turn_server = accountConfiguration.turnConfiguration.server.pjString;
         acc_cfg.turn_cfg.turn_auth_cred.data.static_cred.username = accountConfiguration.turnConfiguration.username.pjString;
-        acc_cfg.turn_cfg.turn_auth_cred.data.static_cred.data_type = (pj_stun_passwd_type) 	accountConfiguration.turnConfiguration.passwordType;
+        acc_cfg.turn_cfg.turn_auth_cred.data.static_cred.data_type = (pj_stun_passwd_type)     accountConfiguration.turnConfiguration.passwordType;
         acc_cfg.turn_cfg.turn_auth_cred.data.static_cred.data = accountConfiguration.turnConfiguration.password.pjString;
     }
     
@@ -176,10 +176,14 @@ static NSString * const VSLAccountErrorDomain = @"VialerSIPLib.VSLAccount";
         acc_cfg.ice_cfg_use = PJSUA_ICE_CONFIG_USE_DEFAULT;
         acc_cfg.ice_cfg.enable_ice = accountConfiguration.iceConfiguration.enableIce;
     }
-
+    
     int accountId;
+    
+    //check if external thread is registered otherwise do so
+    [self checkCurrentThreadIsRegisteredWithPJSUA];
+    
     pj_status_t status = pjsua_acc_add(&acc_cfg, PJ_TRUE, &accountId);
-
+    
     if (status == PJ_SUCCESS) {
         VSLLogInfo(@"Account added succesfully");
         self.accountConfiguration = accountConfiguration;
@@ -188,25 +192,33 @@ static NSString * const VSLAccountErrorDomain = @"VialerSIPLib.VSLAccount";
     } else {
         if (error != NULL) {
             *error = [NSError VSLUnderlyingError:nil
-               localizedDescriptionKey: NSLocalizedString(@"Could not configure VSLAccount", nil)
-           localizedFailureReasonError:[NSString stringWithFormat:NSLocalizedString(@"PJSIP status code: %d", nil), status]
-                           errorDomain:VSLAccountErrorDomain
-                             errorCode:VSLAccountErrorCannotConfigureAccount];
+                         localizedDescriptionKey: NSLocalizedString(@"Could not configure VSLAccount", nil)
+                     localizedFailureReasonError:[NSString stringWithFormat:NSLocalizedString(@"PJSIP status code: %d", nil), status]
+                                     errorDomain:VSLAccountErrorDomain
+                                       errorCode:VSLAccountErrorCannotConfigureAccount];
         }
         return NO;
     }
-
+    
     if (!accountConfiguration.sipRegisterOnAdd) {
         self.accountState = VSLAccountStateOffline;
     }
-
+    
     return YES;
+}
+
+- (void)checkCurrentThreadIsRegisteredWithPJSUA {
+    static pj_thread_desc a_thread_desc;
+    static pj_thread_t *a_thread;
+    if (!pj_thread_is_registered()) {
+        pj_thread_register("VialerPJSIP", a_thread_desc, &a_thread);
+    }
 }
 
 - (void)removeAccount {
     VSLLogVerbose(@"Removing account");
     pj_status_t status;
-
+    
     status = pjsua_acc_del((pjsua_acc_id)self.accountId);
     if (status != PJ_SUCCESS) {
         VSLLogError(@"Unable to remove account from sip server.");
@@ -217,7 +229,7 @@ static NSString * const VSLAccountErrorDomain = @"VialerSIPLib.VSLAccount";
 - (void)registerAccountWithCompletion:(RegistrationCompletionBlock)completion {
     VSLLogDebug(@"Account valid: %@", self.isAccountValid ? @"YES": @"NO");
     VSLLogDebug(@"Should force registration: %@", self.forceRegistration ? @"YES" : @"NO");
-
+    
     if (!self.isAccountValid) {
         VSLLogError(@"Account registration failed, invalid account!");
         NSError *error = [NSError VSLUnderlyingError:nil
@@ -231,10 +243,10 @@ static NSString * const VSLAccountErrorDomain = @"VialerSIPLib.VSLAccount";
         }
         return;
     }
-
+    
     pjsua_acc_info info;
     pjsua_acc_get_info((pjsua_acc_id)self.accountId, &info);
-
+    
     pjsua_acc_config cfg;
     pjsua_acc_get_config((pjsua_acc_id)self.accountId, [VSLEndpoint sharedEndpoint].pjPool, &cfg);
     
@@ -244,11 +256,11 @@ static NSString * const VSLAccountErrorDomain = @"VialerSIPLib.VSLAccount";
     if (self.forceRegistration || (!self.registrationInProgress && info.expires == -1)) {
         self.registrationInProgress = YES;
         VSLLogVerbose(@"Sending registration for account: %@", [NSNumber numberWithInteger:self.accountId]);
-
+        
         pj_status_t status;
         status = pjsua_acc_set_registration((pjsua_acc_id)self.accountId, PJ_TRUE);
         self.registrationInProgress = NO;
-
+        
         if (status != PJ_SUCCESS) {
             VSLLogError(@"Account registration failed");
             NSError *error = [NSError VSLUnderlyingError:nil
@@ -262,7 +274,7 @@ static NSString * const VSLAccountErrorDomain = @"VialerSIPLib.VSLAccount";
         VSLLogVerbose(@"VSLAccount registered or registration in progress, cannot sent another registration");
         VSLLogVerbose(@"VSLAccount state: %ld", (long)self.accountState);
     }
-
+    
     // Check if account is connected, otherwise set completionblock.
     if (self.accountState == VSLAccountStateConnected) {
         completion(YES, nil);
@@ -279,19 +291,19 @@ static NSString * const VSLAccountErrorDomain = @"VialerSIPLib.VSLAccount";
     [desc appendFormat:@"\t Registration Status: %d\n", (int)self.registrationStatus];
     [desc appendFormat:@"\t Registration Expires: %d\n", (int)self.registrationExpiresTime];
     [desc appendFormat:@"\t Account valid %@\n", self.isAccountValid ? @"YES": @"NO"];
-
+    
     return desc;
 }
 
 - (BOOL)unregisterAccount:(NSError * _Nullable __autoreleasing *)error {
-
+    
     if (!self.isRegistered) {
         return YES;
     }
-
+    
     pj_status_t status;
     status = pjsua_acc_set_registration((pjsua_acc_id)self.accountId, PJ_FALSE);
-
+    
     if (status != PJ_SUCCESS) {
         VSLLogError(@"Account unregistration failed");
         if (error != nil) {
@@ -317,9 +329,9 @@ static NSString * const VSLAccountErrorDomain = @"VialerSIPLib.VSLAccount";
 - (void)accountStateChanged {
     pjsua_acc_info accountInfo;
     pjsua_acc_get_info((pjsua_acc_id)self.accountId, &accountInfo);
-
+    
     pjsip_status_code code = accountInfo.status;
-
+    
     if (code == 0 || (code != PJSIP_SC_FORBIDDEN && code != PJSIP_SC_UNAUTHORIZED && accountInfo.expires == -1)) {
         self.accountState = VSLAccountStateDisconnected;
         if (self.shouldReregister) {
@@ -357,10 +369,10 @@ static NSString * const VSLAccountErrorDomain = @"VialerSIPLib.VSLAccount";
             // Post a notification so the user could be informed.
             if (self.registrationCompletionBlock) {
                 NSError *error = [NSError VSLUnderlyingError:nil
-                                    localizedDescriptionKey:NSLocalizedString(@"Account unregistration failed", nil)
-                                localizedFailureReasonError:[NSString stringWithFormat:NSLocalizedString(@"PJSIP status code: %d", nil), code]
-                                                errorDomain:VSLAccountErrorDomain
-                                                  errorCode:VSLAccountErrorRegistrationFailed];
+                                     localizedDescriptionKey:NSLocalizedString(@"Account unregistration failed", nil)
+                                 localizedFailureReasonError:[NSString stringWithFormat:NSLocalizedString(@"PJSIP status code: %d", nil), code]
+                                                 errorDomain:VSLAccountErrorDomain
+                                                   errorCode:VSLAccountErrorRegistrationFailed];
                 self.registrationCompletionBlock(NO, error);
                 self.registrationCompletionBlock = nil;
                 self.registrationInProgress = NO;
@@ -389,7 +401,7 @@ static NSString * const VSLAccountErrorDomain = @"VialerSIPLib.VSLAccount";
 
 - (void)removeCall:(VSLCall *)call {
     [self.callManager removeCall:call];
-
+    
     // All calls are ended, we will unregister the account.
     if ([[self.callManager callsForAccount:self] count] == 0) {
         [self unregisterAccount:nil];
